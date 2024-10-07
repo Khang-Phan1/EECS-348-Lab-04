852106cca4a19a34df082d31ac1c056f9b3316c4
Khang Phan <khangphan0515@gmail.com>
2024-10-06 20:53:02 -0500

    version 0.2, code1

--- code1.c
+++ code1.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
-int safety_count(score) {
+int safety_count(int score) {
     int count = 0;
     while (score >= 2) {
         score -= 2;
         count++;
     }
     return count;
+}
+
+int convert(int TD2, int TD1, int TD, int FG, int S) {
+    printf("%d TD+2, %d TD+1, %d TD, %d FG, %d S\n", TD2, TD1, TD, FG, S);
+    while (S >= 3) {
+        S -= 3;
+        FG += 2;
+        printf("%d TD+2, %d TD+1, %d TD, %d FG, %d S\n", TD2, TD1, TD, FG, S);
+    }
 }
 
 int main() {
@@ -19,32 +28,46 @@ int main() {
             break;
         }
 
-        int temp=score, count=0;
-        while (temp >= 2) {
-            temp -= 2;
-            count++;
-        }
-        if (temp == 1) {
-            count--;
+        S = safety_count(score);
+        if (score%2 == 1) {
+            S--;
             FG++;
         }
-        S = count;
-        printf("%d S, ", S);
-        printf("%d FG\n", FG);
-        
+        convert(TD2, TD1, TD, FG, S);
+
         while (S >= 3) {
             S -= 3;
             FG += 2;
-            printf("%d S, %d FG\n", S, FG);
         }
 
         if (FG >= 2) {
-            temp = score;
-            for (int i; i<=FG/2; i++) {
-                temp -= 6;
+            int temp1 = score;
+            int temp2 = FG;
+            for (int i=0; i<temp2/2; i++) {
+                FG = 0;
+                temp1 -= 6;
                 TD++;
-
+                S = safety_count(temp1);
+                if (temp1%2 == 1) {
+                    S--;
+                    FG++;
+                }
+                convert(TD2, TD1, TD, FG, S);
             }
+        }
+
+        int temp = score;
+        while (temp >= 7) {
+            FG = 0;
+            TD = 0;
+            temp -= 7;
+            TD1++;
+            S = safety_count(temp);
+            if (temp%2 == 1) {
+                S--;
+                FG++;
+            }
+            convert(TD2, TD1, TD, FG, S);
         }
     }
 }
